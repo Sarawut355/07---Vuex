afc86adb13ad94d7c454825585ce5a463a4d11b7
Sarawut355 <75597167+Sarawut355@users.noreply.github.com>
2021-02-20 23:47:31 +0700

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <div class="text-danger">
+      ****กรุณากดยืนยันเพื่อยืนยันสินค้าที่ท่านเลือก****
+    </div>
+    <div class="row">
+      <product
+        v-for="(item, index) in Menuitem"
+        :key="index"
+        :NameProduct="item.NameProduct"
+        :img="item.img"
+        :Price="item.Price"
+        :id="index"
+        @Order="menuorder"
+      />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { mapState } from "vuex";
+import Product from "../components/Product.vue";
+export default {
+  name: "Home",
+  components: {
+    Product,
+  },
+  computed: {
+    ...mapState(["Menuitem"]),
+  },
+};
+</script>
